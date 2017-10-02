8c287eb40419b1525088150a50d76f080995656a
rb <32129393+Malrb@users.noreply.github.com>
2017-10-02 11:46:49 -0500

    Add files via upload

---
+++ src/variable.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    /* Difference with operation speed  */
+    /* CPU register is faster than RAM memory */
+    //Declare variable in CPU register
+    register int reg_cpu;
+    //Declare in RAM memory - the same with "int ram_mem;"
+    auto int ram_mem;
+    //Declare var at outside file for "included file"
+    extern int outside;
+
+    /* Testing the speed with parameter above */
+    for(int i=0;i<10000;i++)
+        cout<<i<<" ";
+    return 0;
+}

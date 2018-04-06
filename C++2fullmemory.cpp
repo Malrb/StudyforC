94698699b4588a22635025951a5e8aea466b749d
rb <32129393+Malrb@users.noreply.github.com>
2018-04-06 19:47:32 +0800

    Update C++2fullmemory.cpp

--- C++2fullmemory.cpp
+++ C++2fullmemory.cpp
@@ -1,10 +1,6 @@
-//前面四行#include開頭的是導入會用到的標頭檔
 
-//第六行的while(1)是為了不斷重複執行
-
-//第七行的malloc(0x100000)是命令系統分配出1M的記憶體
-
-//第八行的WinExec("calc",SW_NORMAL)是開啟計算機
+//malloc(0x100000)是命令系統分配出1M的記憶體
+//WinExec("calc",SW_NORMAL)是開啟計算機
 
 #include<iostream>
 

